fe94023bdd8278ff8cfc6743e8a44ca85b53616c
Ben Pritchard and Lee Jorgensen <ben.s.pritchard@gmail.com>
2015-03-26 10:42:48 -0700

    Add username in devise and bootstrap.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, :presence => true
+  validates :email, :presence => true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "welcometothedungeon", sign_out: "escapetofreedom"}
   root :to => "home#index"
 end
